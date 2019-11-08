e95f4201d013316e0a76ab7650bd805153b2d1d9
OlivierS7 <olivier.simonneau@viacesi.fr>
2019-11-08 16:59:34 +0100

    Regex

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,6 +11,14 @@ class UserController extends Controller
 {
     public function addUser(Request $request)
     {
+        request()->validate([
+            'mail' => 'required|email|max:64',
+            'password' => 'required|min:8|max:32|regex:^(?=.*[A-Z])(?=.*\d).+$^',
+            'passwordVerif' => 'required|same:password' 
+        ]);
+        $user_mail = request('mail');
+        $user_password = request('password');
+
         $client = new \GuzzleHttp\Client();
         $url = "http://localhost:8001/users";
         $response = $client->request('POST', $url, [
