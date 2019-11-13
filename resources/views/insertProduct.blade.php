667c123a283bbeb05d4e4a47b92a24b4e07108c9
OlivierS7 <olivier.simonneau@viacesi.fr>
2019-11-13 16:08:30 +0100

    Css + insertProduct finish

--- resources/views/insertProduct.blade.php
+++ resources/views/insertProduct.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('link')
-
+<link rel="stylesheet" href="{{ asset('css/insertProduct.css') }}">
 @endsection
 
 @section('content')
@@ -14,7 +14,7 @@
                 <form class="form-signin" action="insertDatabaseProduct" method="post" enctype="multipart/form-data">
                     @csrf
                     <div class="form-label-group">
-                        <label for="inputCampus">Catégorie</label>
+                        <label for="inputCampus">Catégorie :</label>
                         <select class="form-control" name="product_category">
                             @foreach ($categories as $category)
                             <option value={{ $category->category_id }}>{{ $category->category_name }}</option>
@@ -22,26 +22,27 @@
                         </select>
                     </div>
                     <div class="form-label-group">
-                        <label>Nom du produit</label>
+                        <label>Nom du produit :</label>
                         <input type="text" name="product_name" class="form-control" placeholder="Nom du produit"
                             required>
                     </div>
                     <div class="form-label-group">
-                        <label>Description du produit (20 caractères minimum)</label>
+                        <label>Description du produit (20 caractères minimum) :</label>
                         <textarea type="text" name="product_description" class="form-control"
                             placeholder="Description du produit" required></textarea>
                     </div>
                     <div class="form-label-group">
-                        <label>Prix du produit</label>
+                        <label>Prix du produit :</label>
                         <input type="text" name="product_price" class="form-control" placeholder="Prix du produit"
                             required>
                     </div>
                     <div class="form-label-group d-flex flex-column">
-                        <label>Image du produit</label>
+                        <label>Image du produit :</label>
                         <input type="file" name="product_image">
                     </div>
 
-                    <button class="btn btn-lg btn-primary btn-block text-uppercase btn-connection" type="submit">Poster l'article</button>
+                    <button class="btn btn-lg btn-primary btn-block text-uppercase btn-connection" type="submit">Poster
+                        l'article</button>
                     @include('flash-message')
                 </form>
             </div>
