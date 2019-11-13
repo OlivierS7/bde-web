8dcb2041f5ab5568b5d6cc0f82e25cc93c74dec3
OlivierS7 <olivier.simonneau@viacesi.fr>
2019-11-13 15:31:38 +0100

    "Add Product + resize"

--- app/Image.php
+++ app/Image.php
@@ -10,27 +10,22 @@ class Image extends Model
     protected $fillable = [
         'event_id', 'image_url', 'user_id',
     ];
-
+    
     public $timestamps= false;
     public $primaryKey = 'image_id';
 
     static function storeImage($imageName)
     {
         $image = $imageName;
-
         $imageExtension = $imageName->getClientOriginalExtension();
-
         $imageName = time() . '.' . $imageExtension;
         $image->storeAs('image', $imageName, 'public');
-
         $imageResize = ImageSize::make(public_path('storage/image/'.$imageName))->resize(300,300)->save();
-
         $storedImage = Image::create([
             'image_url' => $imageName,
             'event_id' => null,
             'user_id' => session('id'),
         ]);
-
         return $storedImage->image_id;
     }
 }
