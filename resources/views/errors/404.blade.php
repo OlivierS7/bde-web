f87010f096aaec80d67614a43bb1148ff7e9e310
OlivierS7 <olivier.simonneau@viacesi.fr>
2019-11-13 16:51:26 +0100

    Error page + css

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+<div class="container">
+    <div class="row d-flex justify-content-center" style="margin-top: 12%">
+        <p style="font-size: 7vh"><strong>Erreur 404 =(</strong></p>
+        <p style="font-size: 7vh">Désolé nous ne trouvons pas la page que vous avez demandée.</p>
+    </div>
+</div>
+@endsection
