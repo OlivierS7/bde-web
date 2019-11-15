4117e268717e4b224ad9644babe629d5047fddca
OlivierS7 <olivier.simonneau@viacesi.fr>
2019-11-15 11:08:07 +0100

    download images

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -12,7 +12,7 @@ class ImageController extends Controller
 {
     public function download()
     {
-        $zip = Zip::create(public_path('images.zip'))->add(public_path('storage/app/public/image/', true))->close();
+        $zip = Zip::create(public_path('images.zip'))->add(public_path('storage/image/', true))->close();
         return Response::download('images.zip')->deleteFileAfterSend(true);
     }
 
