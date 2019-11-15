6f2061889d1173a569336a8a7a52e2d9f161d40e
lloyd <kind.dom.kd@gmail.com>
2019-11-15 09:54:38 +0100

    Produit simple mis en forme

--- resources/views/produit.blade.php
+++ resources/views/produit.blade.php
@@ -3,26 +3,36 @@
     <link rel="stylesheet" href="{{asset('css/produit_simple.css')}}">
 @endsection
 @section('content')
-        <p>Produit: {{ $product->product_name }}</p>
-        <p>Description: {{ $product->product_description }}</p>
-        <p><img src="/storage/image/{{ $product->image->image_url }}"/></p>
-        <p>Prix: {{ $product->product_price }}€</p>
+    <div class="container">
+        <div class="article">
+            <p><strong>{{ $product->product_name }}</strong></p>
+            <p>{{ $product->product_description }}</p>
+
+            <p><img src="/storage/image/{{ $product->image->image_url }}"/></p>
+
+
+            <p>Prix: {{ $product->product_price }}€</p>
+        </div>
         @if(Session::get('status') != null)
-        <form action="{{ route('ajouter', $product->product_id) }}" method="POST">
-            @csrf
-            <input type="submit" value="Ajouter au panier" name="add" />
-        </form>
+            <form action="{{ route('ajouter', $product->product_id) }}" method="POST">
+                @csrf
+                <input type="submit" value="Ajouter au panier" name="add" />
+            </form>
         @endif
+
         @if(Session::get('status') == "Membre BDE")
-        <form action="{{ route('deleteProduct') }}" method="POST">
-            @csrf
-            <input type="hidden" name="id_image" value={{ $product->image->image_id }}>
-            <input type="hidden" name="id_product" value={{ $product->product_id }}>
-            <div class="form-group row">
-                <div class="offset-4 col-8">
-                    <button name="submit" type="submit" class="btn btn-primary" style="background: #17a2b8">Supprimer ce produit</button>
+            <form action="{{ route('deleteProduct') }}" method="POST">
+                @csrf
+                <input type="hidden" name="id_image" value={{ $product->image->image_id }}>
+                <input type="hidden" name="id_product" value={{ $product->product_id }}>
+                <div class="form-group row">
+                    <div class="col-8 mt-1">
+                        <button name="submit" type="submit" class="btn btn-primary" style="background: #17a2b8">Supprimer ce produit</button>
+                    </div>
                 </div>
-            </div>
-        </form>
+            </form>
         @endif
+    </div>
+
+
 @endsection
